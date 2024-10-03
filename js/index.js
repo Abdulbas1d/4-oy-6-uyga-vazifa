d29c57ca1601b58f82c5644f978ce8b4d0d9bf91
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-10-03 21:42:31 +0500

    Any codes write

---
+++ js/index.js
@@ -0,0 +1,129 @@
+// 1-Misol
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+
+
+// 2-Misol
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+
+
+
+// 3-Misol
+
+// let num = +prompt("Sonning kiriting");
+// let kv = num ** 2;
+
+// for (let i = 1; i <= kv; i++) {
+//     console.log(i);
+// }
+
+
+
+
+// 4-Misol
+
+// for (let i = 2; i <= 10; i+=2) {
+//     console.log(i);
+// }
+
+
+
+
+// 5-Misol
+
+// for (let i = 1; i <= 15; i+=2) {
+//     console.log(i);
+// }
+
+
+
+
+// 6-Misol
+
+// for (let i = 1; i <= 5; i++) {
+//     let num = +prompt(`${i}-sonni kiriting`)
+//     console.log(num);
+// }
+
+
+
+
+// 7-Misol
+
+// for (let i = 15; i >= 5; i--) {
+//     console.log(i);
+// }
+
+
+
+
+// 8-Misol
+
+// let num = +prompt("Sonni kiriting");
+
+// for (let i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//         console.log(i);   
+//     }
+// }
+
+
+
+
+// 9-Misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     let num = +prompt(`${i}-sonning kiriting`)
+//     sum += num
+// }
+
+// console.log(sum);
+
+
+
+
+// 10-Misol
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 5 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// 11-Misol
+
+// for (let i = 100; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+
+
+// 12-Misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     sum += i
+// }
+
+// console.log(sum);
+
+
+
+
+// 13-Misol
+
