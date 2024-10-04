02aeb40ee3359b272804e27f11d8a52b26ab30e4
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-10-04 09:34:28 +0500

    For cases solved

--- js/index.js
+++ js/index.js
@@ -127,3 +127,172 @@
 
 // 13-Misol
 
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+
+// for (let i = num1; i <= num2; i++) {
+//     console.log(i);
+// }
+
+
+
+
+// 14-Misol
+
+// let num = +prompt("Sonni kiriting");
+
+// for (let i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// 15-Misol
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i + " " + "ning kvadrati:" + " " + i * i);
+// }
+
+
+
+
+// 16-Misol
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 7 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// 17-Misol
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+
+//     console.log("----------------");
+// }
+
+
+
+
+// 18-Misol
+
+// let num = +prompt("Sonni kiriting");
+// let sum = 0;
+
+// for (let i = 1; i <= num; i++) {
+//     if (num % i == 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+
+
+
+
+// 19-Misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 50; i+=2) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+
+
+
+// 20-Misol
+
+// let num1 = +prompt('1-sonni kiriting');
+// let num2 = +prompt('2-sonni kiriting');
+
+// if (num1 % 2 == 0) {
+//     for (let i = num1; i <= num2; i+=2) {
+//         console.log(i);
+//     }
+// } else {
+//     alert("Bu toq son! Siz juft son kiritishingiz zarur");
+// }
+
+
+
+
+// 21-Misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i+=2) {
+//     sum += i
+// }
+
+// console.log(sum);
+
+
+
+
+// 22-Misol
+
+// let num = +prompt('Sonni kiriting');
+// let sum = 1;
+
+// for (let i = 1; i <= num; i++) {
+//     sum *= i
+// }
+
+// console.log(sum);
+
+
+
+
+// 23-Misol
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 3 != 0 && i % 5 != 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// 24-Misol
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 4 == 0 && i % 6 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// 25-Misol
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let sum = 0;
+
+// if (num1 % 2 == 1) {
+//     if (num2 > num1) {
+//         for (let i = num1; i <= num2; i+=2) {
+//             sum += i
+//         }
+//     } else {
+//         alert("1-soningizdan, 2-soningiz kotta bo'lishi kerak!")
+//     }
+// } else {
+//     alert("Bu Juft son! Siz Toq son kiritishingiz zarur!");
+// }
+
+// console.log(sum);
